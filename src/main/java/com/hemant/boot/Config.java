5d485988e6d67394622bbd5d2d3146fc379b7ec3
hemant <hemantvsn@gmail.com>
2017-04-05 13:22:09 +0530

    Added sysouts

--- src/main/java/com/hemant/boot/Config.java
+++ src/main/java/com/hemant/boot/Config.java
@@ -11,6 +11,13 @@ import org.springframework.context.annotation.Configuration;
 public class Config {
 	
 	public static void main(String[] args) throws Exception {
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
+System.out.println("Now the output is redirected!");
         SpringApplication.run(Config.class, args);
     }
 
